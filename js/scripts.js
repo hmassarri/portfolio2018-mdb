c39ca22f09f58e0b8cdfaa342ba21e5dc7adf423
Hugo Massarri <hugo@hugomassarri.com>
2018-09-12 17:04:25 -0400

    initial layout

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    
+    //Sidenav
+    $('.button-collapse').sideNav();
+    
+    //Sidenav Scrollbar Init
+    var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+    Ps.initialize(sideNavScrollbar);
+});
