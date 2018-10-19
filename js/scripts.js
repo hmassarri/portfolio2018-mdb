1a0fd0665f0d4443093a55a8d657d8a5a250bfcb
Hugo Massarri <hugo@hugomassarri.com>
2018-10-19 17:01:02 -0400

    and more changes because I can't make up my mind

--- js/scripts.js
+++ js/scripts.js
@@ -22,4 +22,28 @@ $(document).ready(function() {
 		}
 	});
     
+
+    // Change color on the nav bar
+    $(window).scroll(function() {
+        //$('.carousel, .carousel-slider').css('opacity', 1 - $(window).scrollTop() / 250);
+        //$('.card1').css('opacity', 1 - $(window).scrollTop() / $('.card1').height() / 2);
+
+        if ($(window).scrollTop() > 30) {
+            $('#topnav').removeClass('white').addClass('black').fadeIn(100);
+            $('.fa-linkedin, .fa-apple, .fa-github, .fa-instagram, .fa-bandcamp, .fa-soundcloud').removeClass('black-text').addClass('white-text').fadeIn(100);
+            $('.breadcrumb-dn').removeClass('black-text').addClass('white-text').fadeIn(100);
+            //$('.link').removeClass('black-text').addClass('white-text');
+            //$('.button-collapse').removeClass('black-text').addClass('white-text');
+            //$('.side-nav').removeClass('white').addClass('pink accent-2');
+        } else {
+            $('#topnav').addClass('white').removeClass('black').fadeIn(100);
+            $('.fa-linkedin, .fa-apple, .fa-github, .fa-instagram, .fa-bandcamp, .fa-soundcloud').removeClass('white-text').addClass('black-text').fadeIn(100);
+            $('.breadcrumb-dn').removeClass('white-text').addClass('black-text').fadeIn(100);
+            //$('.link').removeClass('white-text').addClass('black-text');
+            //$('.button-collapse').removeClass('white-text').addClass('black-text');
+            //$('.side-nav').removeClass('pink accent-2').addClass('white');
+        }
+
+    });
+    
 });
